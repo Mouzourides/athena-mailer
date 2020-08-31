b45e443f9be143e564d069919df5bee088cadb5c
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2020-08-31 01:03:21 +0100

    Handle query failure

--- lambda/main.py
+++ lambda/main.py
@@ -13,29 +13,41 @@ def handler(event, context):
         }
     )
     query_execution_id = query["QueryExecutionId"]
-    check_query_state_until_ready(query_execution_id)
+
+    try:
+        check_query_state_until_ready(query_execution_id)
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": {
+                "message": str(e)
+            }
+        }
 
     query_results = client.get_query_results(QueryExecutionId=query_execution_id)
     result = query_results["ResultSet"]["Rows"][1]["Data"][0]["VarCharValue"]
     print("Total traffic: " + result)
 
-    response_body = {
+    return {
         "statusCode": 200,
         "body": {
             "Total traffic": result
         }
     }
-    return response_body
 
 
 def check_query_state_until_ready(query_execution_id):
     query_exec_results = client.get_query_execution(QueryExecutionId=query_execution_id)
     status_state = query_exec_results['QueryExecution']['Status']['State']
 
-    if (status_state == 'QUEUED' or status_state == 'RUNNING'):
+    if status_state == 'QUEUED' or status_state == 'RUNNING':
         print("Not ready yet...")
         time.sleep(5)
         check_query_state_until_ready(query_execution_id)
+    if status_state == 'FAILED':
+        print(query_exec_results)
+        raise Exception("Failed to execute query: "
+                        + str(query_exec_results["QueryExecution"]["Status"]["StateChangeReason"]))
 
 
 if __name__ == '__main__':
