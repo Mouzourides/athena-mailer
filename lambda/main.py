25f3ee383b1363c5fe82387985701735046472ef
Nik Mouzourides <nikolas.mouzourides@gmail.com>
2020-09-13 15:38:00 +0100

    Add additional queries

--- lambda/main.py
+++ lambda/main.py
@@ -8,23 +8,20 @@ email_client = boto3.client('ses')
 
 
 def handler(event, context):
-    query = athena_client.start_query_execution(
-        QueryString="SELECT COUNT(*) FROM nikmouz_website_logs",
-        ResultConfiguration={
-            "OutputLocation": "s3://nikmouz-athena-query-results"
-        }
-    )
-    query_execution_id = query["QueryExecutionId"]
-
     try:
-        check_query_state_until_ready(query_execution_id)
+        traffic_result = execute_query("SELECT DISTINCT COUNT(request_ip) FROM nikmouz_website_logs")
+        traffic_result_text = "Total unique views: " + traffic_result
+        print(traffic_result_text)
 
-        query_results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
-        result = query_results["ResultSet"]["Rows"][1]["Data"][0]["VarCharValue"]
-        traffic_result = "Total traffic: " + result
-        print(traffic_result)
+        blog_result = execute_query("SELECT count(*) FROM nikmouz_website_logs WHERE uri like '/blog/%'")
+        blog_result_text = "Blog views: " + blog_result
+        print(blog_result_text)
 
-        send_email(traffic_result)
+        art_result = execute_query("SELECT count(*) FROM nikmouz_website_logs WHERE uri like '/art'")
+        art_result_text = "Art views: " + art_result
+        print(art_result_text)
+
+        send_email(traffic_result_text + "\n" + blog_result_text + "\n" + art_result_text)
     except Exception as e:
         return {
             "statusCode": 500,
@@ -39,6 +36,21 @@ def handler(event, context):
             "message": traffic_result
         }
     }
+
+
+def execute_query(query):
+    query = athena_client.start_query_execution(
+        QueryString=query,
+        ResultConfiguration={
+            "OutputLocation": "s3://nikmouz-athena-query-results"
+        }
+    )
+    query_execution_id = query["QueryExecutionId"]
+    check_query_state_until_ready(query_execution_id)
+    query_results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
+    result = query_results["ResultSet"]["Rows"][1]["Data"][0]["VarCharValue"]
+
+    return result
 
 
 def check_query_state_until_ready(query_execution_id):
